be71457cee7af31a6d9354d5e25ec14f7e9cde67
BogdanMoskvitin <moskvitin.bg@gmail.com>
2025-07-16 16:58:22 +0300

    add prod api

--- src/app/services/dishes.service.ts
+++ src/app/services/dishes.service.ts
@@ -2,32 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DishRequest, DishResponse } from '../models/dish.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishesService {
-  private apiUrl = 'http://localhost:3000';  // адрес твоего бэка
-
   constructor(private http: HttpClient) {}
 
   getDishes(): Observable<DishResponse[]> {
-    return this.http.get<DishResponse[]>(`${this.apiUrl}/dishes`);
+    return this.http.get<DishResponse[]>(`${environment.apiUrl}/dishes`);
   }
 
   getDish(id: number): Observable<DishResponse> {
-    return this.http.get<DishResponse>(`${this.apiUrl}/dishes/${id}`);
+    return this.http.get<DishResponse>(`${environment.apiUrl}/dishes/${id}`);
   }
 
   createDish(dish: DishRequest): Observable<DishResponse> {
-    return this.http.post<DishResponse>(`${this.apiUrl}/dishes`, dish);
+    return this.http.post<DishResponse>(`${environment.apiUrl}/dishes`, dish);
   }
 
   updateDish(id: number, dish: Partial<DishRequest>): Observable<DishResponse> {
-    return this.http.put<DishResponse>(`${this.apiUrl}/dishes/${id}`, dish);
+    return this.http.put<DishResponse>(`${environment.apiUrl}/dishes/${id}`, dish);
   }
 
   deleteDish(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/dishes/${id}`);
+    return this.http.delete<void>(`${environment.apiUrl}/dishes/${id}`);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://bv-dishes-back.onrender.com'
 };
